4e4aec49f2ed5ada7ad2abd104a34dfba2441701
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-04 14:12:42 +0900

    feat(*): Turborepo + pnpm 모노레포 프로젝트 생성

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <div>Hello World</div>
+      <Button appName="web">Open alert</Button>
+    </div>
+  );
+}
