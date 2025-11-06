6834846aad02592d925cc3eb3c89bcc329643c6d
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-06 22:23:39 +0900

    feat(web): home 레이아웃 구성

--- apps/web/app/[locale]/page.tsx
+++ apps/web/app/[locale]/page.tsx
@@ -1,12 +1,19 @@
-import { Button } from "@repo/ui/button";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const t = useTranslations("home");
+  const t = useTranslations("applications");
   return (
-    <div>
-      <div className="text-blue-gray-500 text-en-head1">{t("hello")}</div>
-      <Button appName="web">Open alert</Button>
+    <div className="w-full flex-1 py-[7rem] flex flex-col items-center  bg-gray-200">
+      <div className="flex flex-col items-center w-[100rem]">
+        <h2 className="w-full text-center bg-white text-gray-900 text-head1 px-[2rem] py-[3.2rem] mb-[1.75rem]">
+          {t("title")}
+        </h2>
+        <div className="w-full  px-[2rem] py-[3.2rem] bg-white">
+          <div className="text-gray-900 text-head2">
+            {t("techcourse")} {t("web")} {8} {t("generation")}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
