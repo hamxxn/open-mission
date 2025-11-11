375db5756f3cdaa186297c5a14fba33e570e4158
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-11 16:48:31 +0900

    feat(app): mission info section 컴포넌트 구현

---
+++ apps/web/app/[locale]/mission/[id]/(.)section/Section.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import ListHeader from "@/[locale]/mission/[id]/components/list-header";
+
+interface SectionProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export default function Section({ title, children }: SectionProps) {
+  return (
+    <div className="w-full mt-[2.4rem] flex flex-col gap-[1em]">
+      <ListHeader title={title} />
+      <ul className="list-disc list-inside pl-8">{children}</ul>
+    </div>
+  );
+}
