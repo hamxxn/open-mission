c455fc37a722b5cfe118ee02994c047553ed070e
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-17 11:04:17 +0900

    feat(*): message mockup 생성

--- apps/web/app/mock/message-mockup.ts
+++ apps/web/app/mock/message-mockup.ts
@@ -1,5 +1,29 @@
 export const messageMockup = {
   messageId: 1,
   sender: "권동희",
-  message: "안녕하세요, 권동희입니다.",
+  message: "꼭 합격하고 싶습니다!!",
 };
+
+export const articleMockup = [
+  {
+    title: "모노레포 적용과 eslint prettier setting",
+    category: ["모노레포", "eslint prettier"],
+    link: "https://www.notion.so/2a1378a8b738808ab4e4fe8e86d3b19b?pvs=74",
+    backgroundImageWebp: "/assets/article-1.webp",
+    backgroundImage: "/assets/article-1.png",
+  },
+  {
+    title: "다국어 구조",
+    category: ["next-intl", "URL routing", "번역키 관리하기"],
+    link: "https://chip-blob-f2b.notion.site/setting-2a1378a8b73880bab0ace72aa4c6d28f",
+    backgroundImageWebp: "/assets/article-2.webp",
+    backgroundImage: "/assets/article-2.png",
+  },
+  {
+    title: "대역폭 최적화",
+    category: ["데이터 리스트 가상화", "code splitting"],
+    link: "https://chip-blob-f2b.notion.site/2ae378a8b7388070b617e9e20c68d703",
+    backgroundImageWebp: "/assets/article-3.webp",
+    backgroundImage: "/assets/article-3.png",
+  },
+];
